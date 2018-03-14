48160aaa70f5b17f6048103b69cf40687bd92b2a
Destark <bricwuzzy@gmail.com>
2018-03-14 18:46:43 -0500

    Pushing jquery hands on

---
+++ Dev211/Module 4/Module 4.8/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    manipulateDOM();
+});
+
+function manipulateDOM(){
+    var h1Headers = $('h1');
+    h1Headers.text('Hello World');
+
+    var h3Headers = $('h3');
+    h3Headers.css('color', '#37887D');
+    h3Headers.first().css('text-decoration', 'line-through');
+
+    var lastH3HEader = $('h3:last');
+    lastH3HEader.css('background-color', '#53226A');
+}
