c945443f96e1071b9a35cebeb1bbb52454a3f0c6
Destark <bricwuzzy@gmail.com>
2018-03-15 18:43:55 -0500

    Finishing of hands on for the module

---
+++ Dev211/Module 4/Typescript/ts/script.ts
@@ -0,0 +1,16 @@
+class Alert{
+    public showDialog(){
+        var message: string;
+        message = this.getMessage();
+        alert(message);
+    }
+
+    private getMessage(): string{
+        return 'Hello World';
+    }
+}
+
+function showAlert(){
+    var alertManager = new Alert();
+    alertManager.showDialog();
+}
